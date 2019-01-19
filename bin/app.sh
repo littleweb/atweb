8fedd26e674f54807f8c95dddc14014fc95a3191
libin <girlblank@gmail.com>
2019-01-19 11:30:32 +0800

    up

--- bin/app.sh
+++ bin/app.sh
@@ -4,7 +4,8 @@ rm package.json
 rm package-lock.json
 cp /atweb/app/package.json /package.json
 npm i npm -g
-npm i --registry=https://registry.npm.taobao.org
+# npm i --registry=https://registry.npm.taobao.org
+npm i
 # pm2 start /node_modules/atweb/bin/pm2/$1.json -- $2
 pm2 start /node_modules/atweb/bin/pm2/$1.json
 pm2 logs;
